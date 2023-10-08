74d2b99c5795ca24541fb2adce8d27b6483cb1c3
Rivera-py <jaluri@outlook.com>
2023-10-08 20:10:40 +0100

    Adding auth handler init

--- darts_dashboard_tools/authentication.py
+++ darts_dashboard_tools/authentication.py
@@ -1,14 +1,16 @@
 # Google Sheets API authentication
 
-# import os
-# import json
-# from google_auth_oauthlib.flow import InstalledAppFlow
-# from google.oauth2.credentials import Credentials
+import os
+import json
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.oauth2.credentials import Credentials
 
 
 class AuthenticationHandler():
     ''' Google Sheets API authentication class '''
 
-    def __init__():
+    def __init__(self, credential_filepath: str = "credentials.json"):
         ''' Instantiate auth handler '''
-        return None
+        self.scopes = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+        self.credential_filepath = os.path.realpath("credentials.json")
+        return
